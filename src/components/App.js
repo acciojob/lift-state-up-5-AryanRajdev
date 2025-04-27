780470c4de9d45a76678c4e27d004db358aca7a7
Aryan Raj <aryanraj24032002@gmail.com>
2025-04-27 16:14:59 +0530

    third commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 
 import React, { useState } from "react";
 import './../styles/App.css';
-import Form from "./form";
+import Form from "./Form";
 
 const App = () => {
   const [isLoggedIn , setIsLoggedIn] = useState(false);
--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,7 @@ import React from "react";
 const Form = ({ setIsLoggedIn }) => {
 
     function handleSubmit(e){
-       e.preventDefault;
+       e.preventDefault();
        setIsLoggedIn(true);
     }
 
